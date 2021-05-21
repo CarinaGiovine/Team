6a02b6d1ff5c9071f18a2ce3737ca91a9253688f
CarinaGiovine <carinagiovine@gmail.com>
2021-05-21 17:32:55 -0300

    ahora

--- prueba.py
+++ prueba.py
@@ -20,14 +20,10 @@ print("Octavio ESCRIBIO ESTA LINEA 2")
 print("Octavio ESCRIBIO ESTA LINEA 3")
 print("Octavio ESCRIBIO ESTA LINEA 4")
 print("Carina actualiza ahora")
-<<<<<<< HEAD
 print("Creando ramas")
-=======
 print("Octavio agrega una linea a la rama HOY de Carina")
 print("bueno...va otro cambio")
 
-
->>>>>>> 12134d96d49c6db4d585eb7bbe40dffdd5bbceaf
 
 
 
