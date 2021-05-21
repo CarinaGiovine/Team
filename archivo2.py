b5efd78058a0a2e054a994bff91cd7fc18313b90
CarinaGiovine <carinagiovine@gmail.com>
2021-05-21 18:32:10 -0300

    commint nuevo

---
+++ archivo2.py
@@ -0,0 +1,10 @@
+print("Probando mientras hablamos de chivitos y mandarinas")
+def nros(n):
+    print("Creamos numeros")
+    lista=[]
+    for i in range (1,n):
+        lista.append(i**2)
+    print("La lista con los cuadrados es: ",lista)
+    
+nros(5)
+
